33a50b54c4f414f745fe7570be9241e18257c8c8
Manojkumar Ankalkhope <manojankalkhope@outlook.com>
2025-10-30 10:53:13 +0530

    Update main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,1 +1,9 @@
+module "ec2" {
+  source                 = "../../modules/EC2"
+  ami                    = var.ec2_ami
+  instance_type          = var.ec2_instance_type
+  subnet_id              = element(module.vpc.public_subnets, 0)
+  key_name               = var.ssh_key_name
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+}
 
