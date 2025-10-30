7771ad9f62c18dd2f910c11e3de710b3885ecb17
Manojkumar Ankalkhope <manojankalkhope@outlook.com>
2025-10-30 18:05:52 +0530

    Update main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,9 +1,35 @@
-module "ec2" {
-  source                 = "../../modules/EC2"
-  ami                    = var.ec2_ami
-  instance_type          = var.ec2_instance_type
-  subnet_id              = element(module.vpc.public_subnets, 0)
-  key_name               = var.ssh_key_name
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+# Security group example
+resource "aws_security_group" "ec2_sg" {
+  name        = "${var.name}-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-sg"
+  }
 }
 
+module "ec2" {
+  source = "../../modules/ec2"
+
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name               = var.key_name
+  name                   = var.name
+}
