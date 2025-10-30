85ab38645cf5472f8c7125cbdfaf94da1b44df6f
Manojkumar Ankalkhope <manojankalkhope@outlook.com>
2025-10-30 18:32:25 +0530

    Update variable.tf

--- env/dev/variable.tf
+++ env/dev/variable.tf
@@ -2,7 +2,7 @@ variable "region" { default = "ap-south-1" } # Mumbai
 variable "ami" { default = "ami-01760eea5c574eb86" }
 variable "instance_type" { default = "t3.micro" }
 variable "subnet_id" { default = "subnet-01ea549bde3bf4c26" }
-variable "vpc_id" { default = "vpc-0123456789abcdef0" } # change
+variable "vpc_id" { default = "vpc-059cb535523cd3041" } # change
 variable "key_name" { default = "aws_champ-key" }
 variable "name" { default = "dev-ec2" }
 
