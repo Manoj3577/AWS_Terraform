a9ee314183cf03e01b4b41356c1abed9ada70967
Manojkumar Ankalkhope <manojankalkhope@outlook.com>
2025-10-30 18:04:32 +0530

    Update output.tf

--- modules/EC2/output.tf
+++ modules/EC2/output.tf
@@ -1,5 +1,7 @@
-# Outputs
-output "instance_id"  { value = aws_instance.this.id }
-output "public_ip"    { value = aws_instance.this.public_ip }
-output "private_ip"   { value = aws_instance.this.private_ip }
+output "instance_id" {
+  value = aws_instance.this.id
+}
 
+output "public_ip" {
+  value = aws_instance.this.public_ip
+}
