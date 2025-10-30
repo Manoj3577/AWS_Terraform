5c535b88e8f00276fdb2a4832a2d75c27c081aa2
Manojkumar Ankalkhope <manojankalkhope@outlook.com>
2025-10-30 18:07:08 +0530

    Update variable.tf

--- env/dev/variable.tf
+++ env/dev/variable.tf
@@ -1,18 +1,9 @@
-variable "region" { default = "ap-south-1" }  # Mumbai
-variable "name" { default = "dev" }
-variable "ec2_ami" { default = "ami-01760eea5c574eb86" }
-variable "ec2_instance_type" { default = "t3.micro" }
-variable "ssh_key_name" {default = "aws_champ-key" }
+variable "region" { default = "ap-south-1" } # Mumbai
+variable "ami" { default = "ami-01760eea5c574eb86" }
+variable "instance_type" { default = "t3.micro" }
 variable "subnet_id" { default = "subnet-01ea549bde3bf4c26" }
+variable "vpc_id" { default = "vpc-0123456789abcdef0" } # change
+variable "key_name" { default = "aws_champ-key" }
+variable "name" { default = "dev-ec2" }
 
-
-
-# variable "aws_access_key" { sensitive   = true }
-# variable "aws_secret_key" { sensitive   = true }
-# variable "vpc_cidr" {default = 10.0.0.0/16}
-# variable "azs" { default = ["ap-south-1a", "ap-south-1b"] }
-# variable "pbulic_subnet_cidrs" { default = ["10.0.1.0/24","10.0.2.0/24"] }
-# variable "private_subnets_cidrs" { default = ["10.0.101.0/24","10.0.102.0/24"] }
-# variable "admin_cidr" { default = "0.0.0.0/0" }         
-                 
                       
