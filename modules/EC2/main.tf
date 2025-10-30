643ef5ded2c2f35f98dfab913541ed6dac7c73ac
Manojkumar Ankalkhope <manojankalkhope@outlook.com>
2025-10-30 18:03:40 +0530

    Update main.tf

--- modules/EC2/main.tf
+++ modules/EC2/main.tf
@@ -1,13 +1,11 @@
-# EC2 instance creation
 resource "aws_instance" "this" {
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  subnet_id                   = var.subnet_id
-  key_name                    = var.create_new_key ? aws_key_pair.this[0].key_name : var.key_name
-  vpc_security_group_ids      = var.vpc_security_group_ids
-  associate_public_ip_address = true
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+  key_name               = var.key_name
 
   tags = {
-    Name = "public-ec2"
+    Name = var.name
   }
 }
